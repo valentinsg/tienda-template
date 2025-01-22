20b128e998fe50e33b2bb34a11a5b5ab2c753e6d
unknown <sanchezguevaravalentin@gmail.com>
2025-01-21 23:50:51 -0300

    push correcto

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Heading, Text, ListItem, Link, List } from "@chakra-ui/react";
+import { Box, Heading, Text} from "@chakra-ui/react";
 
 const Terms = () => {
   return (
