3523b325925d54296e7b8057a50b8c51c9a1ff3f
unknown <sanchezguevaravalentin@gmail.com>
2025-01-27 23:57:01 -0300

    mejoras de seo 3

--- src/app/products/category/[slug]/page.tsx
+++ src/app/products/category/[slug]/page.tsx
@@ -3,15 +3,18 @@ import React from 'react';
 import { useProducts } from '../../../context/ProductContext';
 import { ProductList } from '../../../components/ProductList';
 import { useRouter } from 'next/navigation';
-import { Flex, Spinner, Text } from '@chakra-ui/react';
+import { Flex, Spinner, Text, Heading, Box } from '@chakra-ui/react';
 import { Product } from '@/types/Product';
+import { useColorMode, useColorModeValue } from '@/app/components/ui/color-mode';
 
 export default function CategoryPage({ params }: { params: Promise<{ slug: string }> }) {
   const resolvedParams = React.use(params);
   const { slug } = resolvedParams;
   const { products, categories, isLoading, error } = useProducts();
   const router = useRouter();
-
+  const textColor = useColorModeValue('#555454', '#D0D0D0');
+  const { colorMode } = useColorMode();
+  
   if (isLoading) {
     return (
       <Flex justify="center" align="center" minH="60vh">
@@ -47,20 +50,14 @@ export default function CategoryPage({ params }: { params: Promise<{ slug: strin
   };
 
   return (
-    <div>
-      <Text
-        fontSize="2xl"
-        fontWeight="bold"
-        textAlign="center"
-        mt={8}
-        mb={4}
-      >
+    <Box bg={colorMode === 'dark' ? 'gray.800' : 'bg.muted'} py={12}  color={textColor}>
+      <Heading as="h1" mb={10} textAlign="center" fontFamily={"Archivo Black"} fontSize={{ base: "4xl", md: "4vw" }} letterSpacing={"tighter"} lineHeight={{ base: 1.2, md: "11vh" }} color={textColor}>
         {category.name}
-      </Text>
+      </Heading>
       <ProductList 
         products={filteredProducts}
         onSelectProduct={handleSelectProduct}
       />
-    </div>
+    </Box>
   );
 }
